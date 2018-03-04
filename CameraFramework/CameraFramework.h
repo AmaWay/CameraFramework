60beb6c1c524c4d0c7465fc7cfd1aba15f4c2bce
Izabela <Izabela@192.168.1.80>
2018-03-04 14:23:04 +0000

    Initial Commit

---
+++ CameraFramework/CameraFramework.h
@@ -0,0 +1,19 @@
+//
+//  CameraFramework.h
+//  CameraFramework
+//
+//  Created by Izabela on 04/03/2018.
+//  Copyright © 2018 AmaWay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CameraFramework.
+FOUNDATION_EXPORT double CameraFrameworkVersionNumber;
+
+//! Project version string for CameraFramework.
+FOUNDATION_EXPORT const unsigned char CameraFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CameraFramework/PublicHeader.h>
+
+
